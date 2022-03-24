ef1898599dce5345c9477f587e9d17c1ef6b237e
Celybot <youancece@gmail.com>
2022-03-24 22:02:32 +0000

    6th function

--- index.js
+++ index.js
@@ -35,3 +35,19 @@ function multiply(num, num2) {
     return num * num2
 }
 console.log(multiply(3, 10))
+
+
+function add(num,num2){
+    console.log("--6--")
+    if (num !== num2){
+        return (num + num2)
+    }
+    
+    return (num + num2) * 3
+    
+};
+console.log(add(2, 4))     //returns 6
+console.log(add(10, -5))   //returns 5
+console.log(add(3, 7.5))   //returns 10.5
+console.log(add(5, 5))     //returns 30 because the sum of 5 + 5 = 10 and 30 is triple that
+console.log(add(6, 6))     //returns 36 because the sum of 6 + 6 = 12 and 36 is triple that
