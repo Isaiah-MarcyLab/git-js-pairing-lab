e14a378415d63740f57c53c3ff39537c96f4c9a9
Celybot <youancece@gmail.com>
2022-03-24 21:07:41 +0000

    second function

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 //Code your solutions in this file
 
 function fiveToOneHundred() {
-    for(let i = 5; i <= 100; i++){
-    console.log(i)
+    for (let i = 5; i <= 100; i++) {
+        console.log(i)
     }
 }
 fiveToOneHundred()
+
+function multiplesOfThree() {
+    for (let i = 1; i <= 100; i++) {
+        if (i % 3 === 0) {
+            console.log(i)
+        }
+    }
+}
+multiplesOfThree()
